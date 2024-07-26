3c6920923c110e0b2a461ae1619b46ecddbc8258
Fekete Balazs Valer <48908633+fbv81bp@users.noreply.github.com>
2024-07-26 22:35:25 +0200

    Update powsum_final.py
    Deleted line 3

--- powsum_final.py
+++ powsum_final.py
@@ -1,6 +1,6 @@
 '''
 Exponents by a smaller primes over the modulus of one larger prime create such permutations that may be shuffled.
-sum
+
 Simply using a shuffle of such permutations would just result in one equal permutation, whose exponent would be the product of all exponents that resulted the sub-permutations.
 
 These sub-permutations are 'compatible' in a sense, that no matter what, however we shuffle the very same set of permutations, the outcome always remains the same. Because of this IFF the discrete logarithm problem can be solved someday, such a series of permutations is not safe on its own by itself.
